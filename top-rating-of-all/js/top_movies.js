adcab6b2bf068080f9b562e1c531db6b679d6fd6
Migueldeve <107196966+Migueldeve@users.noreply.github.com>
2022-09-25 15:26:55 -0500

    Add files via upload

---
+++ top-rating-of-all/js/top_movies.js
@@ -0,0 +1,76 @@
+const TopMovieNetflix = async() => {
+    const respuesta = await fetch('http://localhost:3000/Top_movies_netflix');
+    
+    console.log(respuesta);
+
+    const datos = await respuesta.json();
+    console.log(datos);
+
+    let Netflixs = '';
+    datos.forEach(Netflix => {
+        Netflixs += `<h6>${Netflix.id}</h6>`;
+        Netflixs += `<h5>${Netflix.movie_name}</h5>`;
+    });
+
+    document.getElementById('Netflix').innerHTML = Netflixs;
+}
+
+TopMovieNetflix();
+
+//------------------
+
+const TopMovieHbo = async() => {
+    const respuesta = await fetch('http://localhost:3000/Top_movies_hbo');
+    
+    console.log(respuesta);
+
+    const datos = await respuesta.json();
+    console.log(datos);
+
+    let Hbos = '';
+    datos.forEach(Hbo => {
+        Hbos += `<h6>${Hbo.id}</h6>`;
+        Hbos += `<h5>${Hbo.movie_name}</h5>`;
+    });
+
+    document.getElementById('Hbo').innerHTML = Hbos;
+}
+
+TopMovieHbo();
+
+//---------------
+
+const TopMovieDisneyPlus = async() => {
+    const respuesta = await fetch('http://localhost:3000/Top_movies_disney_plus');
+    
+    console.log(respuesta);
+
+    const datos = await respuesta.json();
+    console.log(datos);
+
+    let Disneys = '';
+    datos.forEach(Disney => {
+        Disneys += `<h6>${Disney.id}</h6>`;
+        Disneys += `<h5>${Disney.movie_name}</h5>`;
+    });
+
+    document.getElementById('Disney').innerHTML = Disneys;
+}
+
+TopMovieDisneyPlus();
+
+
+//varialbles
+
+var ttl1 = "𝕟𝕖𝕥𝕗𝕝𝕚𝕩";
+var ttl2 = "𝕙𝕓𝕠";
+var ttl3 = "𝕕𝕚𝕤𝕟𝕖𝕪 +";
+
+document.getElementById("Titulo1");
+Titulo1.innerHTML = ttl1;
+
+document.getElementById("Titulo2");
+Titulo2.innerHTML = ttl2;
+
+document.getElementById("Titulo3");
+Titulo3.innerHTML = ttl3;
---
+++ top-rating-of-all/top_movies.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Top movies</title>
+    <link rel="stylesheet" href="./css/top_movies.css">
+</head>
+<body>
+    <?php require('./resources/header.html') ?>
+
+    <br>
+
+    <h1><div id="Titulo1"></div></h1>
+
+    <hr width=1400 size=4 color=black>
+
+    <h1><div id="Netflix"><div></h1>
+
+    <br>
+    <br>
+
+    <h1><div id="Titulo2"></div></h1>
+
+    <hr width=1400 size=4 color=black>
+
+    <h1><div id="Hbo"><div></h1>
+
+    <br>
+    <br>
+
+    <h1><div id="Titulo3"></div></h1>
+
+    <hr width=1400 size=4 color=black>
+
+    <h1><div id="Disney"><div></h1>
+
+    <script src="./js/top_movies.js"></script>
+</body>
+</html>
